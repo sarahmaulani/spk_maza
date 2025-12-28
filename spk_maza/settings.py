62d1d07e3701f208a505d7374ffacb52864b09f5
Sarah Maulani <sarahmaulani73@gmail.com>
2025-12-29 00:03:13 +0700

    Update settings.py

--- spk_maza/settings.py
+++ spk_maza/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-auxuh+8=9(%yb&6hpsszc-y)v2x1t9=k1zrnsm33zt!xz96y(9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'nama-aplikasi-anda.vercel.app']
+ALLOWED_HOSTS = ['mazacake.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -118,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+DEBUG = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
